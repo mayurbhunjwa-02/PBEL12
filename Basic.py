29cfe7a97a18515f4c3f94d8d69e1f725d045ddf
mayurbhunjwa-02 <mayurbhunjwa@gmail.com>
2026-06-23 18:02:25 +0530

    added numpy.ipynb

---
+++ Basic.py
@@ -0,0 +1,23 @@
+# variable 
+a=10  # integer variable # dynamic typing
+print(a)
+print(type(a))  
+
+a= 10.5  # float variable
+a="Hello"  # string variable ''  "" """"""
+a=True  # boolean variable
+a=1+1j  # complex variable
+a=1,2,3,4  # tuple variable ()
+a= [1,2,3,4]  # list variable []
+a= {1,2,3,4}  # set variable {}
+a= {1: "one", 2: "two", 3: "three"}  # dictionary variable {}
+
+class MyClass:
+    pass
+
+# 1_data= 100  # variable name starting with number is not allowed
+# _data= 100  # variable name starting with underscore is allowed
+# @data= 100  # variable name starting with @ is not allowed
+# data item= 100  # variable name with space is not allowed
+# if= 100  # variable name with reserved keyword is not allowed
+# set= 100  # variable name with reserved keyword is not allowed
---
+++ README.md
@@ -0,0 +1,1 @@
+discription of AI
